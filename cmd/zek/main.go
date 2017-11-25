a0feb394c1053cc31bd5f293e64c196dee53adeb
Martin Czygan <martin.czygan@gmail.com>
2017-11-25 18:09:45 +0100

    update command

--- cmd/zek/main.go
+++ cmd/zek/main.go
@@ -10,12 +10,22 @@ import (
 	"github.com/miku/zek"
 )
 
+var createExampleProgram = flag.Bool("p", false, "write out an example program")
+
 func main() {
 	flag.Parse()
 
 	root := new(zek.Node)
 	if _, err := root.ReadFrom(os.Stdin); err != nil {
 		log.Fatal(err)
+	}
+
+	if *createExampleProgram {
+		sw := zek.NewStructWriter(os.Stdout)
+		if err := sw.WriteNode(root); err != nil {
+			log.Fatal(err)
+		}
+		os.Exit(0)
 	}
 
 	b, err := json.Marshal(root)
